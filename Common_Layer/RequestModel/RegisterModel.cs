21776d6a9c6c3e6bea49e04f5c1e42e54d8a98cf
Ankit Raj <ankitsingh17072001@gmail.com>
2024-02-26 01:48:57 +0530

    [Ankit] Add : Addition of Request and Response Model for various Controller

---
+++ Common_Layer/RequestModel/RegisterModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Common_Layer.RequestModel
+{
+    public class RegisterModel
+    {
+        public string FName { get; set; }
+        public string LName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+    }
+}
